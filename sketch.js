1f06dfe4442cc500c51868d32fed37609927b40d
cinziamagnani <cinziamagnani@users.noreply.github.com>
2021-07-03 17:36:23 +0200

    drew lines in first quadrant

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-createCanvas(400, 400);
-background(200);
+createCanvas(500, 500);
+  line(width/2,0,width/2,height);
+  line(0, height/2, width, height/2);
 };
 function draw() {
-
+    stroke(127);
+    strokeWeight(10);
+    for (let posX=0; posX < 10; posX++) {
+      line(posX * 25, 0, posX * 25, height/2);
+    }
 };
