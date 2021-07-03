deb23170f22d43d22a2fe31cb0d4ebd816838334
cinziamagnani <cinziamagnani@users.noreply.github.com>
2021-07-03 17:57:51 +0200

    drew second quadrant of canvas

--- sketch.js
+++ sketch.js
@@ -7,6 +7,17 @@ function draw() {
     stroke(127);
     strokeWeight(10);
     for (let posX=0; posX < 10; posX++) {
-      line(posX * 25, 0, posX * 25, height/2);
+      line(posX * 25 + 10, 0, posX * 25 + 10, height/2);
     }
+    noStroke();
+    fill(255, 215, 0);
+    for(let positionX = 0; positionX < 10; positionX++) {
+        for(let positionY = 0; positionY  < 10; positionY ++) {
+            if(positionX % 2 === 0) {
+                circle(width/2 + positionX*25 + 10, positionY *25, 10);
+            } else if(positionX % 2 === 1) {
+                circle(width/2 + positionX*25 + 10, positionY *25 + 10, 10);
+            }
+  }
+}
 };
